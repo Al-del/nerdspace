af2ea82d282ec6baeb75036ee8f609473732aab5
VicBVic <0.botnaruvictor.0@gmail.com>
2023-10-05 23:56:34 +0300

    added basic book profile

--- lib/widgets/book_card_grid_item.dart
+++ lib/widgets/book_card_grid_item.dart
@@ -21,7 +21,13 @@ class BookData {
 
 class BookCardGridItem extends StatelessWidget {
   final BookData data;
-  const BookCardGridItem({super.key, required this.data});
+  final Object heroTag;
+  final void Function() onTap;
+  const BookCardGridItem(
+      {super.key,
+      required this.data,
+      required this.onTap,
+      required this.heroTag});
   final blurValue = 1.0;
   final contentPadding =
       const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0);
@@ -34,47 +40,55 @@ class BookCardGridItem extends StatelessWidget {
     return ClipRect(
       child: BackdropFilter(
         filter: ImageFilter.blur(sigmaX: blurValue, sigmaY: blurValue),
-        child: Card(
-          child: Padding(
-            padding: contentPadding,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ConstrainedBox(
-                  constraints:
-                      BoxConstraints.loose(Size.fromHeight(maxImageHeight)),
-                  child: Padding(
-                    padding: bookPadding,
-                    child: DropshadowImage(
-                      image: data.cover ??
-                          const AssetImage('assets/book_placeholder.jpg'),
-                    ),
-                  ),
-                ),
-                Text(
-                  data.title,
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                Text(
-                  "By ${data.author}",
-                  style: Theme.of(context).textTheme.titleSmall,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Flexible(
-                      child: FittedBox(
-                        child: BookRatingBar(rating: data.rating),
+        child: InkWell(
+          splashColor: Colors.white,
+          highlightColor: Colors.white,
+          onTap: onTap,
+          child: Card(
+            child: Padding(
+              padding: contentPadding,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ConstrainedBox(
+                    constraints:
+                        BoxConstraints.loose(Size.fromHeight(maxImageHeight)),
+                    child: Padding(
+                      padding: bookPadding,
+                      child: Hero(
+                        tag: heroTag,
+                        child: DropshadowImage(
+                          image: data.cover ??
+                              const AssetImage('assets/book_placeholder.jpg'),
+                        ),
                       ),
                     ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.add),
-                    )
-                  ],
-                ),
-              ],
+                  ),
+                  Text(
+                    data.title,
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  Text(
+                    "By ${data.author}",
+                    style: Theme.of(context).textTheme.titleSmall,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Flexible(
+                        child: FittedBox(
+                          child: BookRatingBar(rating: data.rating),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.add),
+                      )
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/widgets/pages/feed.dart
+++ lib/widgets/pages/feed.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:nerdspace/routes/book_profile.dart';
 import 'package:nerdspace/widgets/book_card_grid_item.dart';
 import 'package:nerdspace/widgets/nerdspace_searchbar.dart';
 
@@ -9,11 +10,16 @@ class Feed extends StatelessWidget {
 
   Widget itemBuilder(context, index) {
     return BookCardGridItem(
+        heroTag: index,
+        onTap: () {
+          Navigator.pushNamed(context, '/book_profile',
+              arguments: BookProfileArguments(heroTag: index));
+        },
         data: BookData(
-      title: "The Holy Bible",
-      author: "Anonymous",
-      rating: 4.5,
-    ));
+          title: "The Holy Bible",
+          author: "Anonymous",
+          rating: 4.5,
+        ));
   }
 
   @override
